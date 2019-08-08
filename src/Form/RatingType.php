14bec0cbe2020b88b7a890947ec379bd846c653f
Colitopare <muriel.imbert05@gmail.com>
2019-08-08 08:26:31 +0200

    ajout du read.me

--- src/Form/RatingType.php
+++ src/Form/RatingType.php
@@ -7,17 +7,25 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 
 class RatingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('notation', IntegerType::class, [
+            ->add('notation', ChoiceType::class, [
                 'label' => "Note",
                 'attr' => [
                     'placeholder' => "Votre note pour ce film"
+                ],
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 5,
+                    '5' => 5,
                 ]
             ])
             ->add('comment', TextType::class, [
